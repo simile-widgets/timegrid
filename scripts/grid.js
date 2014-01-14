57de4e493ecf5237769b110951394df2590a6b54
qqliu <quanquan.liu@mit.edu>
2014-01-14 16:44:07 -0500

    Finally fixed jquery bug

--- scripts/grid.js
+++ scripts/grid.js
@@ -20,6 +20,7 @@ Timegrid.Grid = function(objs, xSize, ySize, xMapper, yMapper) {
 
     this.addAll(objs);
 };
+
 $.inherit(Timegrid.Grid, Timegrid.ListenerAware);
 
 Timegrid.Grid.prototype.add = function(obj) {
